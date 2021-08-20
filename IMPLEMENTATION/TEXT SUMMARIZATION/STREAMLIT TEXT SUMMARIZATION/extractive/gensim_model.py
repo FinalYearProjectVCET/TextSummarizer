201610da48151e9215105cd3ccff6aac8b995a59
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2021-08-20 14:57:26 +0530

    gensim v1.03 almost done verification left

--- IMPLEMENTATION/TEXT SUMMARIZATION/STREAMLIT TEXT SUMMARIZATION/extractive/gensim_model.py
+++ IMPLEMENTATION/TEXT SUMMARIZATION/STREAMLIT TEXT SUMMARIZATION/extractive/gensim_model.py
@@ -2,12 +2,15 @@ import gensim
 
 class GensimClass:
 
-    def gensim_method(taketext,takeratio):
+    def gensim_method(self,taketext,takeratio):
         
         text = taketext
-        givenratio = takeratio
+        givenratio = float(takeratio)
         lengthtext=len(text)
+        # print(type(givenratio))
         summarized_content = gensim.summarization.summarize(text, ratio=givenratio)
         print(summarized_content)
         lengthsumm = len(summarized_content)
+        # print(lengthtext)
+        # print(lengthsumm)
         return summarized_content,lengthsumm,lengthtext

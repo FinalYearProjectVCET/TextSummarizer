272b988b3621d53e08ddf5175e64ebed755390d7
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2021-08-20 15:34:20 +0530

    textranktrisl

--- IMPLEMENTATION/TEXT SUMMARIZATION/STREAMLIT TEXT SUMMARIZATION/main.py
+++ IMPLEMENTATION/TEXT SUMMARIZATION/STREAMLIT TEXT SUMMARIZATION/main.py
@@ -1,11 +1,17 @@
 import extractive.gensim_model as gensim_model
+import extractive.textrank_model as textrank_model
+
 def main():
     print("Hello World")
     gettext = input("give text")
     getratio = input("give ratio")
-    gensimobj = gensim_model.GensimClass()
-    gensimobj.gensim_method(gettext,getratio)
-
+    getmodel = input("give model")
+    if getmodel == "gensim":
+        gensimobj = gensim_model.GensimClass()
+        gensimobj.gensim_method(gettext,getratio)
+    elif getmodel == "textrank":
+        gensimobj = textrank_model.TextRankClass()
+        gensimobj.text_rank_method()
 
 if __name__ == '__main__':
     main()
